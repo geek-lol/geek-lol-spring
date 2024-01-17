2f4dddb5c59409a3b2f140da9b0cb06b94887ec9
yongseopK <kk0@kakao.com>
2024-01-17 12:44:36 +0900

    initial commit

---
+++ src/main/java/com/nat/geeklolspring/GeekLolSpringApplication.java
@@ -0,0 +1,13 @@
+package com.nat.geeklolspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekLolSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeekLolSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nat/geeklolspring/GeekLolSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nat.geeklolspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekLolSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

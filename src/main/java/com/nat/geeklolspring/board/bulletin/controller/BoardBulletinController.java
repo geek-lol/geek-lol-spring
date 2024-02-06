c2edbee4952f94dc7f11f912258106aecc27b56e
JoHo99 <ghdi926@naver.com>
2024-02-06 16:27:33 +0900

    자개 파일 업로드 수정

--- src/main/java/com/nat/geeklolspring/board/bulletin/controller/BoardBulletinController.java
+++ src/main/java/com/nat/geeklolspring/board/bulletin/controller/BoardBulletinController.java
@@ -13,14 +13,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 @RestController
@@ -75,7 +80,7 @@ public class BoardBulletinController {
     @PostMapping()
     public ResponseEntity<?> boardCreate( // 생성
             @AuthenticationPrincipal TokenUserInfo userInfo, // 토큰에서 주는 유저 정보
-            @RequestPart("boardInfo")BoardBulletinWriteRequestDTO dto, // 생성시 받을 정보
+            @Validated @RequestPart("boardInfo")BoardBulletinWriteRequestDTO dto, // 생성시 받을 정보
             @RequestPart(value = "fileUrl",required = false) MultipartFile fileUrl, // 게시판 글 내에 파일
             BindingResult result
             ){
@@ -87,17 +92,24 @@ public class BoardBulletinController {
                     .badRequest()
                     .body(result.getFieldError());
         }
-
-        Map<String, String> fileMap = FileUtil.uploadFile(fileUrl, rootFilePath);
-        String filePath = fileMap.get("filePath");
+//        Map<String, String> fileMap = FileUtil.uploadFile(fileUrl, rootFilePath);
+//        String filePath = fileMap.get("filePath");
 
         try {
+            String uploadImagePath = null;
+            if (fileUrl != null) {
+                log.info("file-name: {}",fileUrl.getOriginalFilename());
+                uploadImagePath = boardBulletinService.uploadImage(fileUrl);
+            }
             BoardBulletinDetailResponseDTO responseDTO =
-                    boardBulletinService.create(dto,userInfo,filePath);
+                    boardBulletinService.create(dto,userInfo,uploadImagePath);
             return ResponseEntity.ok().body(responseDTO);
-        }catch (Exception e){
-            log.warn("문제 발생");
+        } catch (IllegalStateException e){
+            log.warn(e.getMessage());
             return ResponseEntity.badRequest().body(e.getMessage());
+        }catch (Exception e){
+            log.warn(e.getMessage());
+            return ResponseEntity.internalServerError().body(e.getMessage());
         }
 
     }
@@ -113,26 +125,32 @@ public class BoardBulletinController {
 
         log.info("userinfo ID : {}",userInfo.getUserId());
         log.info("dto posterId : {}",dto.getPosterId());
-//
+
 //        if (!dto.getPosterId().equals(userInfo.getUserId()) || !userInfo.getRole().toString().equals("ADMIN")){
 //            return ResponseEntity.badRequest().body("수정권한이 없습니다");
 //        }
 
-
-        Map<String, String> fileMap = FileUtil.uploadFile(fileUrl, rootFilePath);
-        String filePath = fileMap.get("filePath");
+//        Map<String, String> fileMap = FileUtil.uploadFile(fileUrl, rootFilePath);
+//        String filePath = fileMap.get("filePath");
 
         try {
+            String uploadImagePath = null;
+            if (fileUrl != null) {
+                log.info("file-name: {}",fileUrl.getOriginalFilename());
+                uploadImagePath = boardBulletinService.uploadImage(fileUrl);
+            }
             BoardBulletinDetailResponseDTO responseDTO =
-                    boardBulletinService.modify(dto,filePath);
+                    boardBulletinService.modify(dto,uploadImagePath);
             return ResponseEntity.ok().body(responseDTO);
-        }catch (Exception e){
-            log.warn("문제 발생");
+        } catch (IllegalStateException e){
+            log.warn(e.getMessage());
             return ResponseEntity.badRequest().body(e.getMessage());
+        }catch (Exception e){
+            log.warn(e.getMessage());
+            return ResponseEntity.internalServerError().body(e.getMessage());
         }
 
     }
-
     @DeleteMapping()
     public void boarddelete( // 삭제
             @AuthenticationPrincipal TokenUserInfo userInfo,
@@ -143,6 +161,60 @@ public class BoardBulletinController {
             log.info("delete board : {}",dto.getTitle());
         }catch (RuntimeException e){
             log.warn(e.getMessage());
+        }
+
+    }
+
+    //이미지 파일 불러오기
+    @GetMapping("/load-profile")
+    public ResponseEntity<?> loadProfile(
+            @Validated String bulletinId
+    ){
+
+        try {
+            String imagePath = boardBulletinService.getImagePath(Long.valueOf(bulletinId));
+
+            File file = new File(imagePath);
+
+            if (!file.exists()) return ResponseEntity.notFound().build();
+
+            byte[] fileData = FileCopyUtils.copyToByteArray(file);
+
+            HttpHeaders headers = new HttpHeaders();
+
+
+            MediaType mediaType = extractFileExtension(imagePath);
+            if (mediaType == null){
+                return ResponseEntity.internalServerError().body("이미지가 아닙니다");
+            }
+
+            headers.setContentType(mediaType);
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .body(fileData);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+
+    }
+
+
+    private MediaType extractFileExtension(String filePath){
+
+        String ext = filePath.substring(filePath.lastIndexOf(".") + 1);
+
+        switch (ext.toUpperCase()) {
+            case "JPEG": case "JPG":
+                return MediaType.IMAGE_JPEG;
+            case "PNG":
+                return MediaType.IMAGE_PNG;
+            case "GIF":
+                return MediaType.IMAGE_GIF;
+            default:
+                return null;
         }
 
     }

07f62b9d9394f9139d0c89ccdd72f33fd377d9bd
NoteSeonjin <hawooks112@gmail.com>
2024-02-16 12:33:08 +0900

    # 에러날 거 같은ㅇ거 throw 처리

--- src/main/java/com/nat/geeklolspring/troll/apply/service/ApplyVoteService.java
+++ src/main/java/com/nat/geeklolspring/troll/apply/service/ApplyVoteService.java
@@ -72,26 +72,39 @@ public class ApplyVoteService {
 
     }
 
+    @Transactional
     public ApplyVoteResponseDTO changeVote(VoteApply vote) {
         BoardApply boardApply = rulingApplyRepository.findById(vote.getVoteId()).orElseThrow();
+        if (boardApply == null){
+            throw new RuntimeException("changeVote : 게시글을 찾을 수 없습니다!");
+        }
         // vote 값 수정
         if (vote.getUp() == 1) {
             rulingApplyRepository.plusUpCount(boardApply.getApplyId());
             vote.setUp(0);
+            // 수정한 vote 값 DB에 저장
+            VoteApply saved = voteCheckRepository.save(vote);
+
+            // 수정된 정보를 저장해서 Controller에 전달
+            return ApplyVoteResponseDTO.builder()
+                    .up(0)
+                    .total(boardApply.getUpCount()+1)
+                    .build();
         }
         else{
             rulingApplyRepository.downUpCount(boardApply.getApplyId());
             vote.setUp(1);
+            // 수정한 vote 값 DB에 저장
+            VoteApply saved = voteCheckRepository.save(vote);
+
+            // 수정된 정보를 저장해서 Controller에 전달
+            return ApplyVoteResponseDTO.builder()
+                    .up(1)
+                    .total(boardApply.getUpCount()-1)
+                    .build();
         }
 
-        // 수정한 vote 값 DB에 저장
-        VoteApply saved = voteCheckRepository.save(vote);
 
-        // 수정된 정보를 저장해서 Controller에 전달
-        return ApplyVoteResponseDTO.builder()
-                .up(saved.getUp())
-                .total(boardApply.getUpCount())
-                .build();
     }
 
     public boolean VoteCheck(ApplyVotePostRequestDTO dto, TokenUserInfo userInfo) {
@@ -108,6 +121,10 @@ public class ApplyVoteService {
     public VoteApply findVote(long applyId, String accountId) {
         BoardApply boardApply = rulingApplyRepository.findById(applyId).orElseThrow();
         User user = userRepository.findById(accountId).orElseThrow();
+
+        if (boardApply == null || user ==null){
+            throw new RuntimeException("user또는 게시글을 찾을 수 없습니다.");
+        }
         // 쇼츠 아이디와 계정명이 일치하는 vote정보를 리턴
         return voteCheckRepository.findByApplyIdAndReceiver(boardApply, user);
     }

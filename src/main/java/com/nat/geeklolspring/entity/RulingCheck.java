5b22a007d9cbc8356d65c9e18cccffb7a1d25c39
NoteSeonjin <hawooks112@gmail.com>
2024-02-16 13:32:42 +0900

    # caseCade 수정

--- src/main/java/com/nat/geeklolspring/entity/RulingCheck.java
+++ src/main/java/com/nat/geeklolspring/entity/RulingCheck.java
@@ -32,7 +32,7 @@ public class RulingCheck {
     @JoinColumn(name = "voter_id")
     private User rulingVoter;
 
-    @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ruling_id")
     private BoardRuling rulingId;
 }
